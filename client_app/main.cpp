d5faec1f0211f85fc6fb149dbc285a8158dccc43
tony <tonyboboc6@gmail.com>
2025-11-29 22:49:41 +0200

    will think if I keep them as 2 sepparate functions

--- client_app/main.cpp
+++ client_app/main.cpp
@@ -2,7 +2,37 @@
 #include <mysql/jdbc.h>  
 #include <iostream>
 #include <memory>
-
+#include <string>
+void show_menu_bar(const std::string & bar_menu,sql::Connection *con){
+        sql::Statement *stmt;
+        sql::ResultSet *res;
+        std::string sql_statement="Select * from "+bar_menu+";";
+        stmt=con->createStatement();
+        res=stmt->executeQuery(sql_statement);
+        while(res->next()){
+            std::cout<<"id= "<<res->getInt(1)<<" ";
+            std::cout<<" | name of drink : "<<res->getString(2);
+            std::cout<<" | price : "<<res->getDouble(3);
+            std::cout<<std::endl;
+        }
+        delete res;
+        delete stmt;
+}
+void show_menu_food(const std::string & food_menu,sql::Connection *con){
+        sql::Statement *stmt;
+        sql::ResultSet *res;
+        std::string sql_statement="Select * from "+food_menu+";";
+        stmt=con->createStatement();
+        res=stmt->executeQuery(sql_statement);
+        while(res->next()){
+            std::cout<<"id= "<<res->getInt(1)<<" ";
+            std::cout<<" | name of drink : "<<res->getString(2);
+            std::cout<<" | price : "<<res->getDouble(3);
+            std::cout<<std::endl;
+        }
+        delete res;
+        delete stmt;
+}
 int main() {
     try {
 
@@ -19,18 +49,9 @@ int main() {
             driver->connect(url, user, pass)
         );
         con->setSchema(schema);
-        sql::Statement *stmt;
-        sql::ResultSet *res;
-        stmt=con->createStatement();
-        res=stmt->executeQuery("Select * from menu_bar;");
-        while(res->next()){
-            std::cout<<"id= "<<res->getInt(1)<<" ";
-            std::cout<<" | name of drink : "<<res->getString(2);
-            std::cout<<" | price : "<<res->getDouble(3);
-            std::cout<<std::endl;
-        }
-        delete res;
-        delete stmt;
+        show_menu_bar(bar_menu,con.get());
+        std::cout<<"now showing food menu\n";
+        show_menu_food(food_menu,con.get());
     } catch (const sql::SQLException &e) {
         std::cerr << "MySQL error: " << e.what()
                   << " (code: " << e.getErrorCode()
